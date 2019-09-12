3e615f89990dead7fa6fb6b741a3a80eedbe9c00
田中　大樹 <tanakadaiki@tanakataijunoMacBook-Pro.local>
2019-09-12 13:52:15 +0900

    [Finish]5-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 
 	end
-	Refile.secret_key = '3ce152b51b75a2191b5b654e789f3fe8ecd679d5fc3706780a2c8302ed3adca9a9d3cdba3c9381ff88b529316de8d8df83cea8da8b8b4609a7a258fd5efb3742'
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+	Refile.secret_key = '3ce152b51b75a2191b5b654e789f3fe8ecd679d5fc3706780a2c8302ed3adca9a9d3cdba3c9381ff88b529316de8d8df83cea8da8b8b4609a7a258fd5efb3742'
